cb7ce58f0668f29fa790d390b64007ef59ed5faa
Luis Flor Chacón <luisflorchacon7@gmail.com>
2024-11-06 20:30:57 +0000

    Generador de miles de dominios realizado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let articulo = ["the", "our", "my", "your", "his", "her", "their"];
+  let adjective = [
+    "green",
+    "blue",
+    "red",
+    "yellow",
+    "orange",
+    "purple",
+    "pink"
+  ];
+  let subject = ["box", "house", "fridge", "car", "window", "bed", "shower"];
+  let domain = [".com", ".es", ".in", ".eu", ".uk", ".us", ".app"];
+  let allDomains = articulo
+    .map(a =>
+      adjective
+        .map(adj =>
+          subject.map(sub => domain.map(dom => a + adj + sub + dom)).flat()
+        )
+        .flat()
+    )
+    .flat();
+  console.log(allDomains);
 };
